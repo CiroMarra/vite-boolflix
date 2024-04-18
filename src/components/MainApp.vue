443f6726c213ee9f88023bfb99516c2ffec0ada7
CiroMarra <tensha93@gmail.com>
2024-04-18 10:44:02 +0200

    fix: add fix

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -13,12 +13,12 @@
 <template>
     <div class="container bg-primary text-white">
         <div class="row d-flex justify-content-between">    
-            <div class="card  my-4 mx-3 px-2" v-for="films in store.movieSearch" style="width: 18rem;">
+            <div class="card  my-4 mx-3 px-2" v-for="film in store.movieSearch" style="width: 18rem;">
                 <img src="..." class="card-img-top" alt="ops...">
                 <div class="card-body">
-                    <h6 class="card-title">Nome: {{ films.title }}</h6>
-                    <p class="card-text">Lingua originale:  <img :src=" `/src/assets/img/${films.original_language}.png`" :alt ="films.original_language" class="ms-img"></p>
-                    <p class="card-text">Voto: {{ Math.ceil(films.vote_average) }}</p>  
+                    <h6 class="card-title">Nome: {{ film.title }}</h6>
+                    <p class="card-text">Lingua originale:  <img :src=" `/src/assets/img/${film.original_language}.png`" :alt ="film.original_language" class="ms-img"></p>
+                    <p class="card-text">Voto: {{ Math.ceil(film.vote_average) }}</p>  
                 </div>
             </div>
         </div> 
