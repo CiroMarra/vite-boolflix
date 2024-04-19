cb738227aad1c66422c239172f4c777f1068573a
CiroMarra <tensha93@gmail.com>
2024-04-19 10:08:44 +0200

    Feat: add Improvement for code

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,9 +1,15 @@
 <script>
     import { store } from "../store.js";
+    import SeriesApp from "./SeriesApp.vue";
+    import MoviesApp from "./MoviesApp.vue";
     
 
     export default{
         name: 'MainApp',
+        components: {
+            SeriesApp,
+            MoviesApp,
+        },
 
         data(){
             return {
@@ -11,52 +17,14 @@
                 flag: ['it', 'en', 'ja']
             }
         },
-        methods: {
-            getImageUrl(flag) {
-                return `./src/assets/img/${flag}.png`;
-            }
-        }
     }
 </script>
 
 <template>
     <div class="container bg-primary text-white">
-        <div class="row d-flex justify-content-between">  
-            <h2>FILMS</h2> 
-            <div class="card  my-4 mx-3 px-2" v-for="films in store.movieSearch" style="width: 18rem;">
-                <img :src="`https://image.tmdb.org/t/p/w342${films.poster_path}`" alt="" class="ms-sfi-mg">
-                <div class="card-body bg-danger">
-                    <h6 class="card-title">Nome: {{ films.title }}</h6>
-                    <div v-if="flag.includes(films.original_language)">
-                        <p class="card-text">Lingua originale: <img :src="getImageUrl(films.original_language)" class="ms-img"></p>
-                    </div>
-                    <div v-else>
-                        Lingua originale: {{ films.original_language }}
-                    </div>   
-                    <p class="card-text">Voto: {{ Math.ceil(films.vote_average) }}</p>  
-                </div>
-            </div>
-        </div> 
-
-
-
-        <div class="row d-flex justify-content-between"> 
-            <h2>Serie TV</h2> 
-            <div class="card  my-4 mx-3 px-2" v-for="series in store.seriesSearch" style="width: 18rem;">
-                <img :src="`https://image.tmdb.org/t/p/w342${series.poster_path}`" alt="" class="ms-sfi-mg">
-                <div class="card-body bg-danger">
-                    <h6 class="card-title">Nome: {{ series.title }}</h6>
-                    <div v-if="flag.includes(series.original_language)">
-                        <p class="card-text">Lingua originale: <img :src="getImageUrl(series.original_language)" class="ms-img"></p>
-                    </div>
-                    <div v-else>
-                        Lingua originale: {{ series.original_language }}
-                    </div>   
-                    <p class="card-text">Voto: {{ Math.ceil(series.vote_average) }}</p>  
-                </div>
-            </div>
-        </div> 
-    </div>  
+        <MoviesApp></MoviesApp>
+        <SeriesApp></SeriesApp>
+    </div>
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/MoviesApp.vue
@@ -0,0 +1,54 @@
+<script>
+    import { store } from "../store.js";
+    
+
+    export default{
+        name: 'MoviesApp',
+
+        data(){
+            return {
+                store,
+                flag: ['it', 'en', 'ja']
+            }
+        },
+
+        methods: {
+            getImageUrl(flag) {
+                return `./src/assets/img/${flag}.png`;
+            }
+        }
+    }
+</script>
+
+<template>
+        <div class="row d-flex justify-content-between">  
+            <h2>FILMS</h2> 
+            <div class="card  my-4 mx-3 px-2" v-for="films in store.movieSearch" style="width: 18rem;">
+                <img :src="`https://image.tmdb.org/t/p/w342${films.poster_path}`" alt="" class="ms-sfi-mg">
+                <div class="card-body bg-danger">
+                    <h6 class="card-title">Nome: {{ films.title }}</h6>
+                    <div v-if="flag.includes(films.original_language)">
+                        <p class="card-text">Lingua originale: <img :src="getImageUrl(films.original_language)" class="ms-img"></p>
+                    </div>
+                    <div v-else>
+                        Lingua originale: {{ films.original_language }}
+                    </div>   
+                    <p class="card-text">Voto: {{ Math.ceil(films.vote_average) }}</p>  
+                </div>
+            </div>
+        </div> 
+</template>
+
+<style scoped lang="scss">
+    
+ .ms-img {
+    width: 16px;
+    height: 12px;
+ }   
+
+ .ms-sfi-mg {
+    object-fit:fill;
+    width: 270px;
+    height: 300px;
+ }
+</style>
